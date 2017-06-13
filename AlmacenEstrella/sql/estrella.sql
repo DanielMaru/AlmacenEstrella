1d9815a7ae0139adebd1e41a60d22261315e9be5
mateoortiz08 <mateo_ortiz08@hotmail.com>
2017-06-13 16:32:15 -0500

    Agrego la base de datos

---
+++ AlmacenEstrella/sql/estrella.sql
@@ -0,0 +1,136 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 13, 2017 at 11:30 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `estrella`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `Id` int(11) NOT NULL,
+  `Nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productos`
+--
+
+CREATE TABLE `productos` (
+  `Id` int(11) NOT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  `FechaIngreso` datetime NOT NULL,
+  `Descripcion` varchar(100) NOT NULL,
+  `Precio` int(11) NOT NULL,
+  `Estado` int(11) NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `IdCategoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productosventas`
+--
+
+CREATE TABLE `productosventas` (
+  `IdProducto` int(11) NOT NULL,
+  `IdVenta` int(11) NOT NULL,
+  `Cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `Id` int(11) NOT NULL,
+  `Cajero` varchar(100) NOT NULL,
+  `FechaVenta` datetime NOT NULL,
+  `Total` int(11) NOT NULL,
+  `Estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `IdCategoria` (`IdCategoria`);
+
+--
+-- Indexes for table `productosventas`
+--
+ALTER TABLE `productosventas`
+  ADD KEY `IdProducto` (`IdProducto`),
+  ADD KEY `IdVenta` (`IdVenta`),
+  ADD KEY `IdVenta_2` (`IdVenta`);
+
+--
+-- Indexes for table `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `categorias`
+--
+ALTER TABLE `categorias`
+  ADD CONSTRAINT `categorias_ibfk_1` FOREIGN KEY (`Id`) REFERENCES `productos` (`IdCategoria`);
+
+--
+-- Constraints for table `productosventas`
+--
+ALTER TABLE `productosventas`
+  ADD CONSTRAINT `productosventas_ibfk_1` FOREIGN KEY (`IdProducto`) REFERENCES `productos` (`Id`),
+  ADD CONSTRAINT `productosventas_ibfk_2` FOREIGN KEY (`IdVenta`) REFERENCES `ventas` (`Id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
