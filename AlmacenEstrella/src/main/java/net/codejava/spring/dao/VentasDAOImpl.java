9a0a200bc3faa1d3d132fbedbc1711dc9735ec39
fercho-01 <luisfernandorzcrzc@gmail.com>
2017-06-14 16:11:56 -0500

    Se implementa el método guardar

--- AlmacenEstrella/src/main/java/net/codejava/spring/dao/VentasDAOImpl.java
+++ AlmacenEstrella/src/main/java/net/codejava/spring/dao/VentasDAOImpl.java
@@ -1,6 +1,7 @@
 package net.codejava.spring.dao;
 
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
@@ -9,32 +10,83 @@ import javax.sql.DataSource;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mysql.cj.api.jdbc.Statement;
+import com.mysql.cj.jdbc.PreparedStatement;
+
+import net.codejava.spring.modelo.Producto;
 import net.codejava.spring.modelo.Venta;
+
+import java.sql.Connection;
+
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+
 
 
 public class VentasDAOImpl implements VentasDAO{
 
 	private JdbcTemplate jdbcTemplate;
 	
+	
+	private ProductoDAO productoDAO;
+	
+	
 	public VentasDAOImpl(DataSource datasource) {
 		jdbcTemplate = new JdbcTemplate(datasource);
+		productoDAO = new ProductoDAOImpl(datasource);
 	}
 	
 	@Transactional
 	@Override
 	public boolean guardar(Venta venta) {
 		boolean retorno = false;
+		long result = 0;
 		if(venta!=null){
+			
+			
 			Date date = new Date();
-			String sql = "INSERT INTO ventas (Cajero, FechaVenta,Total)"
-					+"VALUES(?,?,?)";
-			jdbcTemplate.update(sql,venta.getCajero(),date,venta.getTotal());
+			String sql = "INSERT INTO ventas (Cajero,Total)"
+					+"VALUES(?,?)";
+
 			
-			//Insertar Productos
+			KeyHolder holder = new GeneratedKeyHolder();
+
+			jdbcTemplate.update(new PreparedStatementCreator() {           
+
+			                @Override
+			                public PreparedStatement createPreparedStatement(Connection connection)
+			                        throws SQLException {
+			                    PreparedStatement ps = (PreparedStatement) connection.prepareStatement(sql.toString(), Statement.RETURN_GENERATED_KEYS);
+			                    ps.setString(1, venta.getCajero());
+			                    ps.setInt(2, venta.getTotal());
+			                    return ps;
+			                }
+			            }, holder);
+
+			Long ventaId = holder.getKey().longValue();
 			
+			
+			//Insertar productos
+			venta.getListaProductos().forEach((producto) -> {
+				String sqlProducto = "INSERT INTO productosventas(IdProducto,IdVenta,cantidad) VALUES(?,?,?)";
+				jdbcTemplate.update(sqlProducto,producto.getId(),ventaId,producto.getCantidad());
+				
+				Producto productoBd = productoDAO.obtener(producto.getId());
+				productoBd.setCantidad(productoBd.getCantidad()-producto.getCantidad());
+				productoDAO.guardarOActualizar(productoBd);
+			});
+			retorno = true;
 		}
 		return retorno;
 	}
