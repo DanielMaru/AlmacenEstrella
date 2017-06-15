b0f7d197669106fe6d9437d18c9a5a611c0fc5e4
juanrondonmo <juan.rondonmo@gmail.com>
2017-06-15 15:40:41 -0500

    JavasCript Producto

--- AlmacenEstrella/src/main/webapp/resources/js/productos.js
+++ AlmacenEstrella/src/main/webapp/resources/js/productos.js
@@ -3,7 +3,12 @@ $( document ).ready(function() {
 	$("#btnBuscar").click(function(){
 	    $.ajax({url: "http://localhost:8080/AlmacenEstrella/obtener?id="+$('#idProducto').val(), success: function(result){	       
 	    	   	
-	    	var Producto = result;	        
+	    	var Producto = result;
+	    	$("#nombre").val(Producto.nombre);
+	    	//$("#sel1").val(Producto.sel1);
+	    	$("#descripcion").val(Producto.descripcion);
+	    	$("#precio").val(Producto.precio);
+	    	$("#cantidad").val(Producto.cantidad);
 	        
 	    }});
 	});
